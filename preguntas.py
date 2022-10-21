4b9eecaaf6b72d60b696de894479891c2243d748
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-21 03:35:06 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -82,72 +82,7 @@ def pregunta_03():
     # Importe GridSearchCV
     # Importe Pipeline
     # Importe OneHotEncoder
-    from sklearn.compose import make_column_selector
 
-    from sklearn.compose import make_column_selector
-    from sklearn.compose import make_column_transformer
-    from sklearn.feature_selection import SelectKBest,f_regression
-    from sklearn.linear_model import LinearRegression
-    from sklearn.model_selection import GridSearchCV
-    from sklearn.pipeline import Pipeline
-    from sklearn.preprocessing import OneHotEncoder
-    
-
-   pipeline = Pipeline(
-        steps=[
-            # Paso 1: Construya un column_transformer que aplica OneHotEncoder a las
-            # variables categóricas, y no aplica ninguna transformación al resto de
-            # las variables.
-            (
-                "column_transfomer",
-                make_column_transformer(
-                    (
-                        OneHotEncoder(),
-                        ['sex','smoker','region'],
-                    ),
-                    remainder="passthrough",
-                ),
-            ),
-            # Paso 2: Construya un selector de características que seleccione las K
-            # características más importantes. Utilice la función f_regression.
-            (
-                "selectKBest",
-                SelectKBest(score_func=f_regression),
-            ),
-            # Paso 3: Construya un modelo de regresión lineal.
-            (
-                "linearRegression",
-                LinearRegression(),
-            ),
-        ],
-    )
-
-    # Cargua de las variables.
-    X_train, X_test, y_train, y_test = pregunta_02()
-
-    # Defina un diccionario de parámetros para el GridSearchCV. Se deben
-    # considerar valores desde 1 hasta 11 regresores para el modelo
-    param_grid = {
-        "selectkBest_k": arange(1,12),
-    }
-
-    # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
-    # parámetros. Use cv = 5, y como métrica de evaluación el valor negativo del
-    # error cuadrático medio.
-    gridSearchCV = GridSearchCV(
-        estimator=pipeline,
-        param_grid=param_grid,
-        cv=5,
-        scoring="neg_mean_squared_error",
-        refit=True,
-        return_train_score=True,
-    )
-
-    # Búsque la mejor combinación de regresores
-    gridSearchCV.fit(X_train, y_train)
-
-    # Retorne el mejor modelo
-    return gridSearchCV
 
 def pregunta_04():
     """
