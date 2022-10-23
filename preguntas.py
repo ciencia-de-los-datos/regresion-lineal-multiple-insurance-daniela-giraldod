eff4dae9c5a879eb547688a86b8f553791f5b916
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-23 00:18:50 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -133,7 +133,7 @@ def pregunta_03():
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
     param_grid = {
-        "model__n_jobs": arange(1, 12, 1),
+        "model__n_jobs": np.arange(1, 12, 1),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
