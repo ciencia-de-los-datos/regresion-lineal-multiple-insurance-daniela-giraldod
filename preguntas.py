31881a75934550aab13354cbe1f99482510ff227
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-23 00:08:31 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -103,19 +103,19 @@ def pregunta_03():
             # variables categóricas, y no aplica ninguna transformación al resto de
             # las variables.
             (
-                "column_transfomer",
+         "column_transfomer",
                 make_column_transformer(
                     (
                       OneHotEncoder(),
                       make_column_selector(dtype_include=object),
                     ),
-                    remainder="passthrough",
+                    remainder="drop",
                 ),
             ),
             # Paso 2: Construya un selector de características que seleccione las K
             # características más importantes. Utilice la función f_regression.
             (
-                "SelectKBest",
+                "selectKBest",
                 SelectKBest(score_func=f_regression),
             ),
             # Paso 3: Construya un modelo de regresión lineal.
@@ -132,9 +132,10 @@ def pregunta_03():
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
     param_grid = {
-        "selectkBest_k": range(1,12),
+        "model__n_jobs": np.arange(1, 12, 1),
     }
-
+    
+    
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
     # parámetros. Use cv = 5, y como métrica de evaluación el valor negativo del
     # error cuadrático medio.
