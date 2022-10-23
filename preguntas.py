eb44583828dd1e74ad4502cde6e29a61bfef9c97
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-23 00:20:11 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -98,62 +98,7 @@ def pregunta_03():
 #Cada tupla espera 3 valores separados por comas: primero, el nombre del transformador, que puede ser prácticamente cualquier cosa(pasado como una cadena),
 #segundo es el objeto estimador y el último son las columnas sobre las que deseamos realizar esa operación .
 
-     pipeline = Pipeline(
-        steps=[
-            # Paso 1: Construya un column_transformer que aplica OneHotEncoder a las
-            # variables categóricas, y no aplica ninguna transformación al resto de
-            # las variables.
-            (
-                "column_transfomer",
-                make_column_transformer(
-                    (
-                      OneHotEncoder(),
-                      make_column_selector(dtype_include=object),
-                    ),
-                    remainder="drop",
-                ),
-            ),
-            # Paso 2: Construya un selector de características que seleccione las K
-            # características más importantes. Utilice la función f_regression.
-            (
-                "selectKBest",
-                SelectKBest(score_func=f_regression),
-            ),
-            # Paso 3: Construya un modelo de regresión lineal.
-            (
-                "linearRegression",
-                LinearRegression(),
-            ),
-        ],
-    )
-
-    # Cargua de las variables.
-    X_train, X_test, y_train, y_test = pregunta_02()
-
-    # Defina un diccionario de parámetros para el GridSearchCV. Se deben
-    # considerar valores desde 1 hasta 11 regresores para el modelo
-    param_grid = {
-        "model__n_jobs": np.arange(1, 12, 1),
-    }
-
-    # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
-    # parámetros. Use cv = 5, y como métrica de evaluación el valor negativo del
-    # error cuadrático medio.
-    gridSearchCV = GridSearchCV(
-        estimator=pipeline,
-        param_grid=param_grid,
-        cv=5,
-        scoring="neg_mean_squared_error",
-        refit=True,
-        return_train_score=True,
-    )
-
-    # Búsque la mejor combinación de regresores
-    gridSearchCV.fit(X_train, y_train)
-
-    # Retorne el mejor modelo
-    return gridSearchCV
-
+     
 
 def pregunta_04():
   
